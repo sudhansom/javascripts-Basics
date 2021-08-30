20e20d955c6a05fb84f7927aab75532f0b3adb8b
bhuwan <bkspoudel@gmail.com>
2021-08-30 19:07:45 +0200

    W3resource javascripts basics 1

---
+++ display_time.js
@@ -0,0 +1,14 @@
+//  Write a JavaScript program to display the current day and time in the following format.
+//  Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+var today = new Date();
+var day = today.getDay();
+var day_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+console.log(day_list[day], today)
+var hours = today.getHours()
+var minutes = today.getMinutes()
+var seconds = today.getSeconds()
+var prepand = (hours >= 12)? "PM" : "AM";
+hours = (hours >= 12)? hours - 12 : hours;
+console.log(`Current time is ${hours} : ${minutes} : ${seconds} ${prepand}`)
